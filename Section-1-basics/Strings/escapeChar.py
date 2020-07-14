0f01f1dc229e26f8263870402a36803b07171cc5
Idris Khan <idris.khan303@gmail.com>
2020-07-14 13:01:22 +0100

    Strings

---
+++ Section-1-basics/Strings/escapeChar.py
@@ -0,0 +1,9 @@
+splitString = "This string has been\nsplit ovber\nseveral\nlines"
+print(splitString)
+
+print("""The pet shop owner said "No, no, 'e's uh,...he's resting".""")
+anotherSplitString = """This string has been
+split over
+several
+lines"""
+print(anotherSplitString)
--- Section-1-basics/Strings/strings.py
+++ Section-1-basics/Strings/strings.py
@@ -1,1 +1,4 @@
 print("Today is a good day to learn Python")
+name = input("Please enter your name ")
+greetings = "Hello"
+print("{}, {}".format(greetings, name))
