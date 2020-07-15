1607da677e8c6a1cd6c5a8071370ce441a35ffa5
Idris Khan <idris.khan303@gmail.com>
2020-07-15 12:39:30 +0100

    More on strings

--- Section-1-basics/Strings/escapeChar.py
+++ Section-1-basics/Strings/escapeChar.py
@@ -1,9 +1,20 @@
 splitString = "This string has been\nsplit ovber\nseveral\nlines"
 print(splitString)
 
-print("""The pet shop owner said "No, no, 'e's uh,...he's resting".""")
-anotherSplitString = """This string has been
-split over
-several
+print("""The pet shop owner said "No, no, \
+ 'e's uh,...he's resting".""")
+anotherSplitString = """This string has been \
+split over \
+several \
 lines"""
 print(anotherSplitString)
+
+
+# print("C:\Users\timbuchalka\notes.txt")
+
+print("C:\\Users\\timbuchalka\\notes.txt")
+print(r"C:\Users\timbuchalka\notes.txt")
+
+age = 24
+print(type(age))
+print(type(splitString))
